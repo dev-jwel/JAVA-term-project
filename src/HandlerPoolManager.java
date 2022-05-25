e2f60fb659d677824d5fb0298fa6cece4e146697
jiwon <90748096+qivvoon@users.noreply.github.com>
2022-05-26 00:17:32 +0900

    Create HandlerPoolManager.java

--- src/HandlerPoolManager.java
+++ src/HandlerPoolManager.java
@@ -28,6 +28,7 @@ public class HandlerPoolManager extends Thread {
 	 * 받아온 소켓으로 ChatHander 스레드를 새로이 생성하고 pool에 넣어 관리한다.
 	 */
 	public void addClient(Socket client) {
-		// TODO
+		ChatHander ChatHanderT = new ChatHander(client);
+		handlerPool.add(ChatHanderT);
 	}
 }
