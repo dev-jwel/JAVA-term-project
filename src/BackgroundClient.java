4d1d7ca584a1ff01b64440a1d6aae96bfabba54a
jiwon <90748096+qivvoon@users.noreply.github.com>
2022-06-22 20:51:27 +0900

    Update BackgroundClient.java
    이번에는 아까보다 확실히 디버깅이 실행된 것 같습니다. 그래도 죄송하지만 혹시 모르니 한 번 더 디버깅 해봐주실 수 있으실까요?

--- src/BackgroundClient.java
+++ src/BackgroundClient.java
@@ -1,5 +1,7 @@
 import java.lang.Thread;
 import java.net.Socket;
+import java.time.Duration;
+import java.time.LocalDateTime;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
 import java.util.concurrent.ConcurrentLinkedQueue;
@@ -28,20 +30,24 @@ public class BackgroundClient extends Thread {
 	 * Message 객체 임시저장소이다.
 	 */
 	private ConcurrentLinkedQueue<Message> messageBuffer = new ConcurrentLinkedQueue<Message>();
+	
+	private Socket server;
 
 	/**
 	 * 이 멤버변수는 가장 최근에 데이터를 전송한 시간을 기록한다.
 	 * 클라이언트의 timeout을 막기 위해 사용된다.
 	 */
-	private int recentlySentTime;
+	LocalDateTime recentlySentTime;
 
 	/**
 	 * 이 멤버변수는 가장 최근에 받은 데이터의 수신 시간을 기록한다.
 	 * timeout을 확인하기 위해 사용한다.
 	 */
-	private int recentlyReceivedTime;
+	LocalDateTime recentlyReceivedTime;
 
 	public BackgroundClient(Socket server, ChatClient chatClient) throws IOException {
+		this.server = server;
+		this.chatClient = chatClient;
 		this.outputStream = new ObjectOutputStream(server.getOutputStream());
 		this.receiver = new ChatReceiver(server.getInputStream());
 		this.receiver.start();
@@ -55,14 +61,67 @@ public class BackgroundClient extends Thread {
 	 * 4. receiver로부터 Message를 하나 얻어오고 null이 아니면 ChatClient에 appendMessage()를 통해 보낸다.
 	 */
 	public void run() {
-		// TODO
+		Message MessageObject = new Message();
+		int sendTimeout = 50; //전송 시 timeout을 위한 변수. 50second동안 기다린다.
+		int receiveTimeout = 100; //수신 시 timeout을 위한 변수. 100second동안 기다린다.
+		recentlySentTime = LocalDateTime.now();
+		recentlyReceivedTime = LocalDateTime.now();
+
+		while(true){
+			Message messageBufferMessage = messageBuffer.poll(); //messageBuffer에서 Message를 꺼내 ingoingBufferMessage에 저장
+			Message receiverMessage = receiver.getMessage(); //receiver에서 받은 Message를 receiverMessage에 저장
+			LocalDateTime currentTime = LocalDateTime.now(); //현재 시간측정을 위한 변수
+			Duration betweenSentCurrentSecond = Duration.between(recentlySentTime, currentTime);//recentlySentTime과 currentTime사이의 초 차이
+			Duration betweenReceivedCurrentSecond = Duration.between(recentlyReceivedTime, currentTime);//recentlyReceivedTime과 currentTime사이의 초 차이
+
+			//1
+			if(betweenReceivedCurrentSecond.getSeconds() >= receiveTimeout){
+				System.out.println("[BackgroundClient.run] receive timeout");
+				receiver.interrupt();
+				break;
+			}
+
+			//2
+			if(betweenSentCurrentSecond.getSeconds() >= sendTimeout){
+				System.out.println("[BackgroundClient.run] send timeout");
+				recentlySentTime = LocalDateTime.now();
+				MessageObject.type = MessageType.ALIVE;
+				Object object = (Object)MessageObject;
+				try {
+					outputStream.writeObject(object);
+				} catch (IOException e) {
+					e.printStackTrace();
+					break;
+				}
+			}
+
+			//3
+			if(messageBufferMessage != null){
+				System.out.println("[BackgroundClient.run] message received from ChatClient");
+				recentlySentTime = LocalDateTime.now();
+				Object object = (Object)messageBufferMessage;
+				try {
+					outputStream.writeObject(object);
+				} catch (IOException e) {
+					e.printStackTrace();
+					break;
+				}
+			}
+
+			//4
+			if(receiverMessage != null){
+				System.out.println("[BackgroundClient.run] message received from server");
+				recentlyReceivedTime = LocalDateTime.now();
+				chatClient.appendMessage(receiverMessage);
+			}
+		}
 	}
 
 	/**
 	 * 이 메소드는 ChatClient의 리스너에서 호출된다.
-	 * ingoingBuffer에 메시지를 하나 채운다.
+	 * messageBuffer에 메시지를 하나 채운다.
 	 */
 	public void sendMessage(Message message) {
-		// TODO
+		messageBuffer.offer(message);
 	}
 }
